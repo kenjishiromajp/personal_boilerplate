6fcb736c816a92fd9b984b62a50a9e836392dd2b
Giovanni Kenji Shiroma <kenjishiromajp@gmail.com>
2018-03-09 10:31:36 -0300

    Fixing plurarizations

--- app/containers/TapeteVermelho/selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const selectTapeteVermelhos = (state) => state.get('tapeteVermelhos');
-export const makeSelectTapeteVermelhos = () => createSelector(
-  selectTapeteVermelhos,
-  (tapeteVermelhoState) => tapeteVermelhoState.get('tapeteVermelhos')
-);
